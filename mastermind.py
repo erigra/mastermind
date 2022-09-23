2170d78236ff3d85e55ed6e891a1970721451911
erigra <erikgrande71@gmail.com>
2022-09-23 23:41:17 +0200

    musbevegelse

--- mastermind.py
+++ mastermind.py
@@ -3,12 +3,16 @@ import random
 
 # Colors
 WHITE= (255,255,255)
+BLACK = (0, 0, 0)
+
 RED = (128, 0, 0)
 GREEN = (0, 128, 0)
 BLUE = (0, 0, 255)
 PURPLE = (128, 0, 255)
-BLACK = (0, 0, 0)
 YELLOW = (255,255,0)
+
+COLORS = {1:RED, 2:GREEN, 3:BLUE, 4:PURPLE, 5:YELLOW}
+
 
 # Board setup
 WIDTH, HEIGHT = 400, 800
@@ -16,18 +20,38 @@ BOARD = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Mastermind")
 
 
+# Functions ::::::::::::::::::::::::::::::::::::::::::::::::::::
+def draw_peg(color, pos):
+    pygame.draw.circle(BOARD, color, pos, 20)
+    
 
-def draw_board():
+
+def draw_board(pos): 
     BOARD.fill(BLACK)
+    pygame.draw.circle(BOARD, RED, pos, 20)
+  #  draw_peg(RED, pos)
     pygame.display.update()
 
 
+def handle_mouse_movement(mousepressed):
+    if mousepressed[pygame.MOUSEBUTTONDOWN]:
+        pos = pygame.mouse.get_pos()
+        return pos
+        
 
+
+def setup_correct():
+    solution = [BLACK, BLACK, BLACK, BLACK]
+    for n in range(4):
+        solution[n]= COLORS[random.randint(1,5)]
+    return solution
 
 
 # Main program starts here :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 def main():
     clock = pygame.time.Clock()
+    position= (100,100)
+
 
     run = True 
     while run:
@@ -38,13 +62,13 @@ def main():
                 pygame.quit()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                print (mouse_pos)
+               # mouse_pressed = pygame.key.get_pressed()
+               # if mouse_pressed:
+                    position = pygame.mouse.get_pos()
         
-            
-
-
-        draw_board()
+        
+        
+        draw_board(position)
 
 
 
