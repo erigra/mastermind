44e766b8d6ec34e877c5ce5d81e69d2ba859f099
erigra <erikgrande71@gmail.com>
2022-09-25 10:31:42 +0200

    so far

--- mastermind2.py
+++ mastermind2.py
@@ -1,12 +1,20 @@
 import pygame
 import random
 
+# Things to do/figure out
 
 
+# Find excact position of rects  (manually or? ) ,
+
+
+
+# Grid radene er hovedobjektene  i spilllet, spilleren vil interagere med en slik gjennom spillets gang...
 class Grid_Row():
     def __init__(self):
         self.fields = [COLORS[1],COLORS[1],COLORS[1],COLORS[1]]
         self.feedback = (0,0,0,0)
+        self.has_border= False
+        self.fill = False
 
     def change_field_color(self, fieldnr, color):
         self.fields[fieldnr] = color
@@ -16,12 +24,23 @@ class Grid_Row():
 
     def draw(self, SCREEN, row):
         for i in range(4):
-            peg = pygame.Rect(((60*i)+20 , (60*row) +20), PEG_SIZE )
-            pygame.draw.rect(SCREEN, self.return_color(i), peg)
+            if self.fill:
+                peg = pygame.Rect(((60*i)+20 , (60*row) +20), PEG_SIZE )
+                pygame.draw.rect(SCREEN, self.return_color(i), peg, width = 0, border_radius=15)
+            else:
+                peg = pygame.Rect(((60*i)+20 , (60*row) +20), PEG_SIZE )
+                pygame.draw.rect(SCREEN, self.return_color(i), peg, width = 2, border_radius=15)
+        
+        if self.has_border:
+            border= pygame.Rect(10,(60*row)+10,240,60)
+            pygame.draw.rect(SCREEN, WHITE, border, width = 1)
 
 
 
 # Colors
+BOARD_COLOR1= (139,69,19) # Brown board background
+BOARD_COLOR2= (102,51,0)   # Darker brown for unused pegholes
+
 WHITE= (255,255,255)
 BLACK = (0, 0, 0)
 RED = (128, 0, 0)
@@ -30,7 +49,8 @@ BLUE = (0, 0, 255)
 PURPLE = (128, 0, 255)
 YELLOW = (255,255,0)
 
-COLORS = [BLACK, RED, GREEN, BLUE, PURPLE, YELLOW]
+COLORS = [BOARD_COLOR1, BOARD_COLOR2, WHITE, BLACK, RED, GREEN, BLUE, PURPLE, YELLOW]
+
 
 # Global variables
 PEG_SIZE = (40,40)
@@ -70,7 +90,14 @@ def main():
     board[4].change_field_color(2, GREEN)
     board[7].change_field_color(1, PURPLE)
 
+
+
+    SCREEN.fill(BOARD_COLOR1)
+
+
     for i in range(10):
+        if i == 5: board[i].has_border = True
+        if i == 2: board[i].fill = True
         board[i].draw(SCREEN, i)
 
     
